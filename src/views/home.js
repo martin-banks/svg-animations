26c8f3e2c5ec0cbac75aa4ac0a1c7394bf8a248c
Martin Banks <anotherbanksy@gmail.com>
2019-04-08 23:46:10 +1000

    adds home page view

---
+++ src/views/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Styled from 'styled-components'
+
+class ViewHome extends Component {
+
+  render () {
+    return (
+      <h1>Home page</h1>
+    )
+  }
+}
+
+export default ViewHome
