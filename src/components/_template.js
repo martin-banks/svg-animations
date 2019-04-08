7773423600ed2a0cced010b19f8d44219f5b12e9
Martin Banks <anotherbanksy@gmail.com>
2019-04-08 23:45:35 +1000

    adds basic component template

---
+++ src/components/_template.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Styled from 'styled-components'
+
+class ComponentName extends Component {
+
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default ComponentName
