4f98a6b254e7f1f9c35c193d536a7bbc2b312c1a
Martin Banks <anotherbanksy@gmail.com>
2019-04-09 22:16:27 +1000

    adds secondary ring

--- src/components/svg/marching-ants/index.js
+++ src/components/svg/marching-ants/index.js
@@ -8,19 +8,19 @@ const Svg = Styled.svg`
   transform: translate(-50%, 0);
 `
 
-function drawPath (pathLength) {
+function drawPath (pathLength, rotation) {
   return keyframes`
   0% {
-    stroke-dashoffset: ${pathLength * 1};
-    stroke: red;
+    stroke-dashoffset: ${pathLength * 1 * rotation};
+    stroke: cyan;
   }
   50% {
     stroke-dashoffset: 0;
-    stroke: gold;
+    stroke: hotpink;
   }
   100% {
-    stroke-dashoffset: ${pathLength * -1};
-    stroke: red;
+    stroke-dashoffset: ${pathLength * -1 * rotation};
+    stroke: cyan;
   }
 `
 }
@@ -29,13 +29,14 @@ const Circle = Styled.circle`
   fill: none;
   stroke-width: 10px;
   stroke-dasharray: ${p => p.circleLength};
-  animation: ${p => drawPath(p.circleLength)} linear 5s infinite;
+  animation: ${p => drawPath(p.circleLength, p.rotation)} linear 5s infinite;
 `
 
 export default class extends Component {
   constructor (props) {
     super(props)
     this.refCircle = React.createRef()
+    this.refCircle_2 = React.createRef()
     this.state = {
       circleLength: 0,
       dynamicLength: 0,
@@ -44,33 +45,35 @@ export default class extends Component {
       modifier: 1,
     }
 
-    this.updateCircleLength = this.updateCircleLength.bind(this)
+    // this.updateCircleLength = this.updateCircleLength.bind(this)
   }
 
-  updateCircleLength () {
-    const { circleLength, dynamicLength, lengthMin, lengthMax, modifier } = this.state
-    let newDynaimcLength = dynamicLength
-    let newModifier = modifier
-    if (modifier === 1 && dynamicLength > (lengthMax - 10)) {
-      newModifier = -1
-    } else if (modifier === -1 && dynamicLength < (lengthMin + 10)) {
-      newModifier = 1
-    } else {
-      newModifier = modifier
-    }
-    newDynaimcLength += (1 * newModifier)
+  // updateCircleLength () {
+  //   const { circleLength, dynamicLength, lengthMin, lengthMax, modifier } = this.state
+  //   let newDynaimcLength = dynamicLength
+  //   let newModifier = modifier
+  //   if (modifier === 1 && dynamicLength > (lengthMax - 10)) {
+  //     newModifier = -1
+  //   } else if (modifier === -1 && dynamicLength < (lengthMin + 10)) {
+  //     newModifier = 1
+  //   } else {
+  //     newModifier = modifier
+  //   }
+  //   newDynaimcLength += (1 * newModifier)
 
-    this.setState({
-      modifier: newModifier,
-      dynamicLength: newDynaimcLength
-    })
-    window.requestAnimationFrame(this.updateCircleLength)
-  }
+  //   this.setState({
+  //     modifier: newModifier,
+  //     dynamicLength: newDynaimcLength
+  //   })
+  //   window.requestAnimationFrame(this.updateCircleLength)
+  // }
 
   componentDidMount () {
     const pathLength = this.refCircle.current.getTotalLength()
+    const pathLength_2 = this.refCircle_2.current.getTotalLength()
     this.setState({
       circleLength: pathLength,
+      circleLength_2: pathLength_2,
       dynamicLength: pathLength / 10,
       lengthMax: pathLength / 10,
       lengthMin: pathLength / 25,
@@ -89,11 +92,18 @@ export default class extends Component {
             cx="150"
             cy="150"
             r="150"
+            rotation="1"
             circleLength={ this.state.circleLength / 10 }
           />
+          <Circle
+            ref={ this.refCircle_2 }
+            cx="150"
+            cy="150"
+            r="100"
+            rotation="-3"
+            circleLength={ this.state.circleLength_2 / 6 }
+          />
         </Svg>
-
-        {/* <h1>{ this.state.dynamicLength }</h1> */}
 
       </div>
     )
